bc0ddc436d64e32255ffaeb7395e67a560960f4d
antoine <antoine.gayoso@gmail.com>
2021-11-23 11:46:43 +0100

    feature: base config

--- src/config/env.js
+++ src/config/env.js
@@ -1,21 +1,14 @@
 import dotenv from 'dotenv';
-// import { Sequelize } from 'sequelize';
-
 dotenv.config();
 
 const config = {
     dbUser: process.env.DB_USER,
     dbPassword: process.env.DB_PASSWORD,
     dbName: process.env.DB_NAME,
-    dbPort: process.env.DB_PORT,
+    dbPort: process.env.DB_PORT || 3000,
     dbHost: process.env.DB_HOST,
     jwtSecret: process.env.JWT_SECRET,
     appPort: process.env.APP_PORT || 8080,
 };
 
-// const sequlize = new Sequelize(
-//     config.dbName,
-//     config.dbUser,
-//     config.dbPassword,
-//     { dialect: 'mysql', port: config.dbPort, host: config.dbHost }
-// )
+export default config;
